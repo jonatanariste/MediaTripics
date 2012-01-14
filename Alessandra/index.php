9db96ed24dde20bf8214170f3351b395a25cf2ea
Jonatan Ariste <jonatan.ariste@gmail.com>
2012-01-14 15:03:05 -0300

    Test loco
    modifique el conecto.php

--- Alessandra/index.php
+++ Alessandra/index.php
@@ -1,3 +1,3 @@
 <?php
 require_once('mobile_device_detect.php');
-mobile_device_detect(true,true,true,true,true,true,true,'main.html','main2.htm');
+mobile_device_detect(true,true,true,true,true,true,true,'main.html','main2.htm');mobile_device_detect(true,true,true,true,true,true,true,'main.html','main2.htm');
