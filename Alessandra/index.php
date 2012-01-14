88c820521be2f29f0536dbfbb24c3e9c6c32068c
Jonatan Ariste <jonatan.ariste@gmail.com>
2012-01-14 15:08:46 -0300

    Revert 9db96ed24dde20bf8214170f3351b395a25cf2ea^..HEAD

--- Alessandra/index.php
+++ Alessandra/index.php
@@ -1,4 +1,8 @@
 <?php
 require_once('mobile_device_detect.php');
+<<<<<<< HEAD
 mobile_device_detect(true,true,true,true,true,true,true,'main.html','main2.htm');mobile_device_detect(true,true,true,true,true,true,true,'main.html','main2.htm');
 ///COMENTARIO
+=======
+mobile_device_detect(true,true,true,true,true,true,true,'main.html','main2.htm');
+>>>>>>> parent of 9db96ed... Test loco
